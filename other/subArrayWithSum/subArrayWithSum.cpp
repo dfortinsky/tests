418015417cba4e9a0636a8769dfe4f8a583d5265
Dmitriy Fortinskiy <curse561@yandex.ru>
2020-02-20 13:01:56 +0300

    Add advanced approach for findSubArrayWithSum

--- other/subArrayWithSum/subArrayWithSum.cpp
+++ other/subArrayWithSum/subArrayWithSum.cpp
@@ -1,8 +1,6 @@
 #include <unordered_map>
 #include <vector>
 
-#include <boost/container_hash/hash.hpp>
-
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
@@ -13,46 +11,27 @@ using namespace ::testing;
 #ifndef INTUITIVE
 
 pair<int, int> findSubArray(const vector<int>& nums, const int& sum = 0) {
-	// Store subsum by indexes from and to:
-	// memo[[from, to]] = subsum; 
-	unordered_map<pair<int, int>, int, boost::hash<pair<int, int> > > memo;
-	memo.reserve(nums.size() * nums.size() / 2);
+	unordered_map<int, int> memo;
+	memo.reserve(nums.size());
 	auto it = memo.begin();
 
-	int previous = 0;
-	int subsum = 0; // store already calculated subarray here
+	int subsum = 0,
+		required = 0;
 	for (int i = 0; i < nums.size(); ++i) {
-		// If current element is already equal sum (lucky) return it
-		if (nums[i] == sum) {
-			return {i, i};
+		subsum += nums[i];
+		if (subsum == sum) {
+			return {0, i};
 		}
 
-		// Check subarray [i - 1, i]
-		// if ok - return,
-		// else store for future
-		previous = i - 1; // next to current element in order to merge it to array
-		if (previous < 0) {
-			continue;
+		required = subsum - sum;
+		if ((it = memo.find(required)) != memo.end()) {
+			return {it->second + 1, i};
 		}
 
-		if ((subsum = nums[previous] + nums[i]) == sum) {
-			return {previous, i};
-		}
-
-		memo.emplace(pair<int,int>(previous, i), subsum);
-
-		// Let's extend subarray to the left
-		for(int j = previous - 1; j >= 0; --j) {
-			it = memo.find(pair<int,int>(j, previous));
-			if (it != memo.end() && (subsum = it->second + nums[i]) == sum) {
-				return {j, i};
-			}
-
-			memo.emplace(pair<int, int>(j, i), subsum);
-		}
+		memo.emplace(subsum, i);
 	}
 
-	return pair<int,int>(-1,-1);
+	return {-1, -1};
 }
 
 #else
@@ -110,7 +89,7 @@ TEST(FindSubArray, Negative_EmptyArray) {
 }
 
 TEST(FindSubArray, Negative_UnexistingSum) {
-	vector<int> nums (10000, 1);
+	vector<int> nums (1000000, 1);
 	const auto& p = findSubArray(nums, -5);
 
 	EXPECT_THAT(p, Eq(pair<int,int>(-1, -1)));
